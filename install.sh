2cef42da0bbd8e7fd206a419cfca9c87bb403a07
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2024-03-30 23:04:17 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit 1
+fi
+
+rm -rf /var/lib/containers
+mkdir -p /home/.steamos/var/lib/containers
+
+wget -P /etc/systemd/system https://raw.githubusercontent.com/dnkmmr69420/steamos-offload-containers/main/var-lib-containers.mount
+
+systemctl enable var-lib-containers.mount --now
